ae628253f5fcfc695a3acde815513eabb1a74220
APJ-9 <akhil.jose@vonnue.com>
2022-10-19 10:16:49 +0530

    initial commit changed the header

---
+++ etsy/src/Components/Main/EditorsPick.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import MapImges from './MapImges'
+
+function EditorsPick(props) {
+    
+    const data = props.data
+    console.log(data)
+  return (
+    <div className='editors-pick'> 
+    </div>
+  )
+}
+
+export default EditorsPick
--- etsy/src/Components/Main/Main.js
+++ etsy/src/Components/Main/Main.js
@@ -2,14 +2,17 @@ import React from 'react'
 import Discover from './Discover'
 import data from '../../dataBase.json'
 import RecentlyViewed from './RecentlyViewed'
+import EditorsPick from './EditorsPick'
 
 function Main() {
     const discover = data.discover
     const recentlyViewed = data.recentlyViewed
+    const editorsPick = data.editorsPick
   return (
     <>
         <Discover discover={discover}></Discover>
         <RecentlyViewed recentlyViewed={recentlyViewed}></RecentlyViewed>
+        <EditorsPick data={editorsPick}></EditorsPick>
     </>
   )
 }
