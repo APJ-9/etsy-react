411c13957f91347531c21cbee85847adbc748f5b
APJ-9 <akhil.jose@vonnue.com>
2022-10-26 09:43:35 +0530

    started etsy main first commit

---
+++ etsymain/src/components/header/MenuIcon.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './MenuIcon.css'
+
+function MenuIcon() {
+    return (
+        <div className='menu-icon'>
+            <div className="menu-icon-container">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" focusable="false">
+                    <path d="M20 7H4c-.6 0-1-.4-1-1s.4-1 1-1h16c.6 0 1 .4 1 1s-.4 1-1 1zm-4.8 6H4c-.6 0-1-.4-1-1s.4-1 1-1h11.2c.6 0 1 .4 1 1s-.4 1-1 1zm4.8 6H4c-.6 0-1-.4-1-1s.4-1 1-1h16c.6 0 1 .4 1 1s-.4 1-1 1z"></path>
+                </svg>
+            </div>
+        </div>
+    )
+}
+
+export default MenuIcon
